bcea80a50c5af993f9b5aed37ba83d631ad3f985
Samfishergit <samfisher.1990@yahoo.com>
2017-09-13 17:21:27 +0300

    Slider, main page done

--- js/abaanadolu.js
+++ js/abaanadolu.js
@@ -1,19 +1,30 @@
 (function($) {
     "use strict"; // Start of use strict
 
+    $('.single-item').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      autoplay: false,
+      autoplaySpeed: 1500,
+      fade: true,
+      cssEase: 'linear'
+  });
+
     // Hide/Show Footer
     $('div.footer-switch').bind('click', function(event) {
     	console.log('clicked');
     	$("html, body").animate({ scrollTop: $(document).height() }, "fast", function() {
     		if ($("div.footer-site-map").hasClass('footer-revealed')) {
-	    		$( "div.footer-site-map" ).slideDown( "slow" );
-    		} else {
-	    		$( "div.footer-site-map" ).slideUp( "slow" );
-    		}
-    		$("div.footer-site-map").removeClass('footer-hidden');		
-    	});   
-		$( "div.footer-site-map" ).toggleClass('footer-revealed');
-		$( ".footer-switch .fa" ).toggleClass('fa-hidden');
-    });
+               $( "div.footer-site-map" ).slideDown( "slow" );
+           } else {
+               $( "div.footer-site-map" ).slideUp( "slow" );
+           }
+           $("div.footer-site-map").removeClass('footer-hidden');		
+       });   
+      $( "div.footer-site-map" ).toggleClass('footer-revealed');
+      $( ".footer-switch .fa" ).toggleClass('fa-hidden');
+  });
 
 })(jQuery); // End of use strict
+
